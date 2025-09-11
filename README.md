b23d94a7d149d3b0b4bb8c798523768facfdecd4
Robotz213 <nicholas@robotz.dev>
2025-09-11 18:50:39 -0300

    feat: initialize Tauri application with basic setup and Vue integration
    - Added Cargo.toml for Rust dependencies and configuration.
    - Created build.rs for Tauri build process.
    - Defined default capabilities in JSON format.
    - Included various icon assets for application branding.
    - Implemented main Rust library with a greeting command.
    - Set up main entry point for the Tauri application.
    - Configured Tauri settings in tauri.conf.json.
    - Developed Vue components for the application interface.
    - Established Vue router for navigation.
    - Created initial login view.
    - Configured TypeScript settings for the project.
    - Set up Vite configuration for development and build processes.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Projeto JurisREM - Sistema de Gerenciamento de Processos Jurídicos
+
+Esse é o repositório oficial do JurisREM, um sistema de gerenciamento de processos jurídicos desenvolvido para facilitar a organização e o acompanhamento de casos legais.
+
+## Funcionalidades
+
+- Cadastro de processos
+- Acompanhamento de prazos
+- Geração de relatórios
+- Integração com sistemas externos
+
+## Tecnologias Utilizadas
+
+- Tauri
+- Vue.js
+- TypeScript
+- Vite
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+import { ask } from "@tauri-apps/plugin-dialog";
+import TitlebarView from "./components/TitlebarView.vue";
+
+onMounted(async () => {
+  const answer = await ask("This action cannot be reverted. Are you sure?", {
+    title: "Tauri",
+    kind: "warning",
+  });
+
+  console.log(answer);
+});
+</script>
+
+<template>
+  <TitlebarView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </Transition>
+  </RouterView>
+</template>
