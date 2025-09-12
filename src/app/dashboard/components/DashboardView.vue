8d4dcd47714e7caf57a3481215ed4c982f9dbbc0
Robotz213 <nicholas@robotz.dev>
2025-09-12 14:05:22 -0300

    ✨ *feat: adicionar componente de visualização de cartões*
    Adiciona um novo componente de visualização de cartões que exibe informações em quatro cartões distintos com estilos e links para detalhes.
    
    Files Changed:
    - *src/app/dashboard/components/CardsView.vue*: novo componente criado para exibir cartões com informações e links.
    - *src/app/dashboard/components/DashboardView.vue*: integra o novo componente de visualização de cartões na estrutura existente.

--- src/app/dashboard/components/DashboardView.vue
+++ src/app/dashboard/components/DashboardView.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardsView from "./CardsView.vue";
+</script>
 
 <template>
-  <div>
-    <div class="card">
-      <div class="card-header">teste</div>
-      <div class="card-body">
-        <RouterLink :to="{ name: 'login' }"> login</RouterLink>
+  <div class="card" style="min-height: 100%">
+    <div class="card-header">teste</div>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-12">
+          <CardsView />
+        </div>
+        <div class="col-12">
+          <RouterLink :to="{ name: 'login' }"> login</RouterLink>
+        </div>
       </div>
     </div>
   </div>
