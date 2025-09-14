f23bc7da6f9749c9d02afb12f051e82d7c802eff
Robotz213 <nicholas@robotz.dev>
2025-09-13 21:23:54 -0300

    ✨ *feat: adicionar funcionalidade de desabilitar o menu de contexto*
    Adiciona um script que desabilita o menu de contexto ao clicar com o botão direito do mouse na página.
    
    Files Changed:
    - *src/assets/js/disable-context.js*: novo arquivo que impede a abertura do menu de contexto.
    - *src/main.ts*: importa o novo script para a aplicação.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "@/assets/js/disable-context";
 import Aura from "@primeuix/themes/aura";
 import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -6,6 +7,7 @@ import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
 const app = createApp(App);
 const pinia = createPinia();
 
